48648d1e90189b28dfbe2f763b80e0b14eb10636
Fawmi-2006 <fawmijailabdeen@gmail.com>
2026-02-04 12:44:36 +0530

    Extra Data

--- database-scripts/09_add_profile_images.sql
+++ database-scripts/09_add_profile_images.sql
@@ -1,5 +1,3 @@
--- Add profile_image column to staff table
 ALTER TABLE staff ADD COLUMN profile_image VARCHAR(500) DEFAULT NULL;
 
--- Add profile_image column to customers table
 ALTER TABLE customers ADD COLUMN profile_image VARCHAR(500) DEFAULT NULL;
